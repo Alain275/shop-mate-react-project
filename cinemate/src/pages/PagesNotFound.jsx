d3e1a67b38a13b3d95b53a091391aa247a225b3b
Alain275 <alainshema275@gmail.com>
2025-02-18 09:32:00 +0200

    workin with page not found

--- cinemate/src/pages/PagesNotFound.jsx
+++ cinemate/src/pages/PagesNotFound.jsx
@@ -1,6 +1,16 @@
+import pagenotefound from '../assets/images/erik-mclean-Rg-fYTQ5Rgw-unsplash.jpg';
 function PagesNotFound() {
   return (
-    <main>PagesNotFound</main>
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my">
+          <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">404,Oops!</p>
+          <img src={pagenotefound} className="m-2  h-96 w-96 object-cover" alt="Cinemate Logo" />
+        </div>
+        <div className="">
+        </div>
+      </section>
+    </main>
   )
 }
 
